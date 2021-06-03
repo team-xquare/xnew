41c10744e954cd4541e0f85dd35c14628c00f9bd
Eungyeol <eungyeole@naver.com>
2021-06-03 17:12:34 +0900

    ⚡️ :: index 명령어 import

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var commander_1 = __importDefault(require("commander"));
+var commands_1 = __importDefault(require("./commands"));
 commander_1.default
     .alias('xnew')
     .usage('')
-    .arguments('<option> <name>')
-    .option('-L --library', '새로운 라이브러리 추가')
-    .option('-S --service', '새로운 서비스 추가')
-    .action(function (name) {
-    console.log(name);
-})
+    .arguments('[option]')
+    .option('-L --library <name>', '새로운 라이브러리 추가', commands_1.default.library)
+    .option('-S --service <name>', '새로운 서비스 추가', commands_1.default.service)
     .parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import commander from 'commander'
-
+import commands from './commands'
 
 commander
     .alias('xnew')
     .usage('')
-    .arguments('<option> <name>')
-    .option('-L --library','새로운 라이브러리 추가')
-    .option('-S --service','새로운 서비스 추가')
-    .action((name)=>{
-        console.log(name)
-    })
+    .arguments('[option]')
+    .option('-L --library <name>','새로운 라이브러리 추가', commands.library)
+    .option('-S --service <name>','새로운 서비스 추가', commands.service)
     .parse(process.argv)
 
