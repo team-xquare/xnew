71b79ad2c6fec0ca18db95ec93186fc1daa6a23b
Eungyeol <eungyeole@naver.com>
2021-06-03 23:13:59 +0900

    ⚡️ :: [commands] service 수정

--- src/commands/service/service.ts
+++ src/commands/service/service.ts
@@ -1,9 +1,48 @@
 import fs from 'fs'
-function Service(name : string){
-    fs.writeFile('package.json',`{\n\t"name": "@service/${name}",\n\t"version": "1.0.0"\n}`,
-        (err)=>{
-        }
-    )
+import os from 'os';
+import path from 'path';
+import cpy from 'cpy'
+import inquirer from 'inquirer'
 
+async function createXFrontEnd(name : string){
+    const answers = await inquirer.prompt({
+        type: "list",
+        name: "frontend-template",
+        message: "사용할 템플릿을 선택하세요",
+        choices: [
+            {
+                name: "next + typescript",
+                value: "ts-next"
+            },
+            {
+                name: "react + typescript",
+                value: "ts-react"
+            },
+            {
+                name: "react",
+                value: "js-react"
+            }
+        ]
+    })
+    const root = path.resolve();
+    const packageJson = {
+        name : `@service/${name}`,
+        version : '1.0.0',
+
+    }
+    fs.writeFileSync(
+        path.join(root, 'package.json'),
+        JSON.stringify(packageJson, null, 2) + os.EOL
+    );
+    await cpy('**',root, {
+        parents: true,
+        cwd: path.join(__dirname, '../../../src/templates', answers['frontend-template'])
+    })
+    
+}
+
+
+function Service(name : string){
+    createXFrontEnd(name)
 }
 export default Service
