f325142b3fa0e89f493d8cbec0e261953b82ac11
Eungyeol <eungyeole@naver.com>
2021-06-03 17:13:12 +0900

    ⚡️ :: [commands] library 매개변수 추가

---
+++ dist/commands/library/library.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Library(name) {
+    console.log(name);
+}
+exports.default = Library;
---
+++ src/commands/library/library.ts
@@ -0,0 +1,4 @@
+function Library(name : string){
+    console.log(name)
+}
+export default Library
